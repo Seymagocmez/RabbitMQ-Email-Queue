6de626a545dc0362ca3f72a63c977b4e08dcedca
Şeyma G <104601325+Seymagocmez@users.noreply.github.com>
2024-09-17 16:55:17 +0300

    I am really commites to this.

---
+++ MessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VbtInternEmailQueue
+{
+    public class MessageModel
+    {
+        public String mail { get; set; }
+        public String message { get; set; } = "RabbitMQ Sending Email Queue, VBT Software Internship Project, Şeyma Göçmez. Thanks for viewing and listening :)";
+        public String subject { get; set; } = "Şeyma Göçmez, Rabbit MQ Project";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using VbtInternEmailQueue;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest",
+            Port = 5672,
+        };
+
+        using (var connection = factory.CreateConnection()) // 1st using block for connection
+        {
+            using (var channel = connection.CreateModel()) // 2nd using block for channel
+            {
+                channel.QueueDeclare("mail-kuyruk",
+                         durable: true,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+                List<string> emailAddresses = new List<string>
+                {
+                    "gocmezseyma@gmail.com",
+                    "seymagocmez00@gmail.com",
+                    "gocmezbanu@gmail.com"
+                };
+
+                List<MessageModel> messageModels = new List<MessageModel>();
+
+                MessageModel model = new MessageModel();
+                model.mail = "gocmezseyma@gmail.com";
+                messageModels.Add(model);
+
+                foreach (var mail in emailAddresses)
+                {
+                    MessageModel model2 = new MessageModel();
+                    model2.mail = mail;
+                    model2.message = $"{model2.message} - {Guid.NewGuid()}"; // Generate a unique message
+                    messageModels.Add(model2);
+                }
+
+                // Now, this part is still inside the 'channel' scope, so you can use the channel here
+                foreach (var messages in messageModels)
+                {
+                    var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(messages));
+                    channel.BasicPublish(string.Empty, "mail-kuyruk", null, body); // 'channel' is in scope here
+                }
+            } // Channel gets disposed here, but we are done using it and it helps to free up resources. 
+        }
+    }
+}
